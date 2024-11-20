8861f369ca90e6e8dc0a3cbc42e024c7f5e8e71c
jessiacb <106203043+jessiacb@users.noreply.github.com>
2024-11-20 12:53:45 -0500

    app update

--- restaurant-matcher/src/App.js
+++ restaurant-matcher/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import SwipePage from './pages/SwipePage';
+import MatchPage from './pages/MatchPage';
+import ProfilePage from './pages/ProfilePage';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/swipe" element={<SwipePage />} />
+          <Route path="/match" element={<MatchPage />} />
+          <Route path="/profile" element={<ProfilePage />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
