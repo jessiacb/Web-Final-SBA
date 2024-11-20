c07d010b1c2bb08dffacc7e0af7302c5b4f8ab5e
jessiacb <106203043+jessiacb@users.noreply.github.com>
2024-11-20 13:01:53 -0500

    axios

--- restaurant-matcher/src/pages/SwipePage.js
+++ restaurant-matcher/src/pages/SwipePage.js
@@ -1,5 +1,15 @@
+import RestaurantCard from '../components/RestaurantCard';
+
 const SwipePage = () => {
-    return <div>Swipe Page</div>;
-  };
-  
-  export default SwipePage;
+  const restaurants = [ /* Mock Data or Fetch from API */ ];
+
+  return (
+    <div>
+      {restaurants.map((r) => (
+        <RestaurantCard key={r.id} name={r.name} image={r.image} description={r.description} />
+      ))}
+    </div>
+  );
+};
+
+export default SwipePage;
